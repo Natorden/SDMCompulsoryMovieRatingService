8b54c9629adae3df7038fdeef04c3c9c796e123a
Natorden <71587437+Natorden@users.noreply.github.com>
2021-10-04 09:11:13 +0200

    small change to assign test number to test 5

--- SDMCompulsoryMovieRatingService.Core/Service/MovieRatingService.cs
+++ SDMCompulsoryMovieRatingService.Core/Service/MovieRatingService.cs
@@ -14,7 +14,6 @@ namespace SDMCompulsoryMovieRatingService.Core.Service
             _movieRatingRepo = movieRatingRepository;
         }
         
-        
         //todo im adding todos then a number to show which number test this is in correlation to assignment pdf for easier navigation
         
         //todo method 1
@@ -26,7 +25,6 @@ namespace SDMCompulsoryMovieRatingService.Core.Service
         //todo method 2
         public double GetAverageRateFromReviewer(int reviewer)
         {
-            //todo make this code work, right now it takes grade sum from test and divides it by how many lines of code there is when you do the datastore.Add method, we need it to only divide by the reviewer that assigned the rating, not all of them
             var numOfRew = 0;
             var rewSum = 0;
             foreach (var r in _movieRatingRepo.GetAll().Cast<MovieRating>().Where(r => r.Reviewer == reviewer))
@@ -50,6 +48,7 @@ namespace SDMCompulsoryMovieRatingService.Core.Service
             return _movieRatingRepo.GetAll().Cast<MovieRating>().Count(r => r.Movie == movie);
         }
 
+        //todo method 5
         public double GetAverageRateOfMovie(int movie)
         {
             int numOfRew = 0;
@@ -67,12 +66,6 @@ namespace SDMCompulsoryMovieRatingService.Core.Service
             return average;
         }
 
-        //todo method 5
-        // public double GetAverageRateOfMovie(int movie)
-        // {
-        //     throw new System.NotImplementedException();
-        // }
-        
         //todo method 6
         public int GetNumberOfRates(int movie, int rate)
         {
