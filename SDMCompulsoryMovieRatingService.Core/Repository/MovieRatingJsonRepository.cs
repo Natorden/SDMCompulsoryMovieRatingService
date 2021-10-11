488df11c19452f807a84d6b23a2353abbb33713b
Danylo <danyloboshagora@gmail.com>
2021-10-11 10:08:04 +0200

    link no workie

---
+++ SDMCompulsoryMovieRatingService.Core/Repository/MovieRatingJsonRepository.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using SDMCompulsoryMovieRatingService.Core.Model;
+
+namespace SDMCompulsoryMovieRatingService.Core.Repository
+{
+  public class MovieRatingJsonRepository : FileReaderRepository
+  {
+    public MovieRatingJsonRepository(TextReader reader) : base(reader)
+    {
+
+    }
+
+    public override MovieRating ReadOneItem(JsonTextReader reader)
+    {
+      reader.Read();
+      int r = (int)reader.ReadAsInt32();
+
+      reader.Read();
+      int m = (int) reader.ReadAsInt32();
+
+      reader.Read();
+      int g = (int) reader.ReadAsInt32();
+
+      reader.Read();
+      DateTime d = (DateTime) reader.ReadAsDateTime();
+
+      return new MovieRating(r, m, g, d);
+    }
+  }
+}
--- SDMCompulsoryMovieRatingService.Ui/Program.cs
+++ SDMCompulsoryMovieRatingService.Ui/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using SDMCompulsoryMovieRatingService.Core.IService;
+using SDMCompulsoryMovieRatingService.Core.Model;
+using SDMCompulsoryMovieRatingService.Core.Repository;
 using SDMCompulsoryMovieRatingService.Core.Service;
 
 namespace SDMCompulsoryMovieRatingService.Ui
@@ -7,9 +11,10 @@ namespace SDMCompulsoryMovieRatingService.Ui
     {
         private static void Main(string[] args)
         {
-            JsonReader reader = new JsonReader();
-            var list = reader.GetAll();
+            IMovieRatingRepository repo = new MovieRatingJsonRepository(new StreamReader("src/SDMCompulsoryMovieRatingService.Core/ratings.json"));
 
+            var list = repo.GetAll();
+            
             foreach (var mR in list)
             {
                 Console.WriteLine($"{mR.Reviewer} / {mR.Movie} / {mR.Grade} / {mR.ReviewDate}");
